1d783ac1e98cac7a697113e9b82bd7dc3242513b
navnelson7 <navnelson7@gmail.com>
2020-06-14 23:11:55 -0600

    Inicializando repositorio proyecto buscar letras de canciones con API

---
+++ README.md
@@ -0,0 +1,5 @@
+## Buscador de letras de canciones utilizando una API
+Proyecto creacto con create-react-app para buscar letras de canciones.
+
+## Mas descricion
+proximamente
---
+++ src/components/Formulario.js
@@ -0,0 +1,48 @@
+import React from 'react';
+const Formulario = () => {
+    return ( 
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                    >
+                        <fieldset>
+                            <legend className="text-center">Buscador de Canciones</legend>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Artista</label>
+                                        <input 
+                                            type="text"
+                                            className="form-control"
+                                            name="artista"
+                                            placeholder="nombre artista"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Cancion</label>
+                                        <input 
+                                            type="text"
+                                            className="form-control"
+                                            name="cancion"
+                                            placeholder="nombre cancion"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary float-right"
+                            ></button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Formulario;
