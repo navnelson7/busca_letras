3b466af5152ff7622f6ed54e6cab7378836c75f4
navnelson7 <navnelson7@gmail.com>
2020-06-28 23:47:30 -0600

    Agregando states para guargar la busqueda de canciones

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,44 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 const Formulario = () => {
+    const [ busqueda, guardarBusqueda ] = useState({
+        artista: '',
+        cancion: ''
+    }) 
+
+    const [error, guardarError] = useState(false);
+    const {artista, cancion} = busqueda;
+
+    //funcion a cada input para leer su contenido
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        });
+    }
+    //consultar las apis
+    const buscarinformacion = e => {
+        e.preventDefault();
+        if(artista.trim() === '' || cancion.trim() === ''){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+        //todo bien pasar al componente principal
+    }
     return ( 
         <div className="bg-info">
             <div className="container">
                 <div className="row">
+                    <div className="col card text-white bg-transparent mb-5 pt-5 pb-2">
                     <form
-                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
-                    >
-                        <fieldset>
-                            <legend className="text-center">Buscador de Canciones</legend>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="form-group">
-                                        <label>Artista</label>
-                                        <input 
-                                            type="text"
-                                            className="form-control"
-                                            name="artista"
-                                            placeholder="nombre artista"
-                                        />
+                        onSubmit={buscarinformacion}
+                        >
+                            <fieldset>
+                                <legend className="text-center">Buscador de Canciones</legend>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <label>Artista</label>
+                                            <input 
+                                                type="text"
+                                                className="form-control"
+                                                name="artista"
+                                                placeholder="nombre artista"
+                                                onChange={actualizarState}
+                                                value={artista}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <label>Cancion</label>
+                                            <input 
+                                                type="text"
+                                                className="form-control"
+                                                name="cancion"
+                                                placeholder="nombre cancion"
+                                                onChange={actualizarState}
+                                                value={cancion}
+                                            />
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="col-md-6">
-                                    <div className="form-group">
-                                        <label>Cancion</label>
-                                        <input 
-                                            type="text"
-                                            className="form-control"
-                                            name="cancion"
-                                            placeholder="nombre cancion"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <button
-                                type="submit"
-                                className="btn btn-primary float-right"
-                            ></button>
-                        </fieldset>
-                    </form>
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary \ float-right"
+                                >Buscar</button>
+                            </fieldset>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
