79524174234fd8ad1f70648d7861ce38926fe79d
navnelson7 <navnelson7@gmail.com>
2020-06-30 22:06:06 -0600

    Obtenido letra de cancion de api lyrics.ovh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, useState, useEffect} from 'react';
 import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
+import axios from 'axios';
 
 
 function App() {
+  //dfinir el state
+  const [busquedaletra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState('');
+  //usando useEffect
+  useEffect(() => {
+    if( Object.keys(busquedaletra).length === 0) return
+    const consultarApiLetra = async () =>  {
+      const {artista,cancion} = busquedaletra ;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      
+      const resultado = await axios(url);
+      guardarLetra(resultado.data.lyrics);
+    }
+    consultarApiLetra();
+  },[busquedaletra])
   return (
     <Fragment>
-      <Formulario />
+      <Formulario 
+        guardarBusquedaLetra = {guardarBusquedaLetra}
+      />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+
+          </div>
+          <div className="col-md-6">
+            <Cancion 
+              letra = {letra}
+            />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-const Formulario = () => {
+const Formulario = ({guardarBusquedaLetra}) => {
     const [ busqueda, guardarBusqueda ] = useState({
         artista: '',
         cancion: ''
@@ -24,12 +24,14 @@ const Formulario = () => {
         }
         guardarError(false);
         //todo bien pasar al componente principal
+        guardarBusquedaLetra(busqueda);
     }
     return ( 
         <div className="bg-info">
             <div className="container">
                 <div className="row">
                     <div className="col card text-white bg-transparent mb-5 pt-5 pb-2">
+                        {error ? <p className="alert alert-danger text-center p-2">Todos los campos son obligaotiros</p> : null }
                     <form
                         onSubmit={buscarinformacion}
                         >
